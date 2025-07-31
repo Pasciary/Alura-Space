bdee20dcdc1079b1d127e99e2ca28592c3c78d2a
João Pachere <jppachere.fmoura@gmail.com>
2025-07-31 13:25:29 -0300

    Adicionando menu de busca

--- galeria/views.py
+++ galeria/views.py
@@ -11,3 +11,15 @@ def index(request):
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk= foto_id)
     return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
+
+
+def buscar(request):
+    fotografias = Fotografia.objects.order_by("data_reg").filter(publicada=True)
+
+    if "buscar" in request.GET:
+        nome = request.GET['buscar']
+        if nome:
+            # filtra todos os objetos que buscamos para o que conter o que digitar
+            fotografias = fotografias.filter(nome__icontains=nome) 
+
+    return render(request, "galeria/buscar.html", {"cards": fotografias})
